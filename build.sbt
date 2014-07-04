5b9e3a786800a57259c25b03967dfefd19625504
Abu Raihan Md Rahat Hossain <rahat09@yahoo.com>
2014-07-05 09:14:13 +1000

    file logging issue fixed

--- build.sbt
+++ build.sbt
@@ -9,11 +9,15 @@ libraryDependencies +=
   
 libraryDependencies += "com.ning" % "async-http-client" % "1.7.5"
 
-libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.13"
-
 libraryDependencies += "com.typesafe.akka" % "akka-testkit_2.10" % "2.3.3"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.7"
+
+libraryDependencies += "com.typesafe.akka" % "akka-slf4j_2.10" % "2.3.3"
+
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.13"
 
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
 
--- src/main/scala/simple/Getter.scala
+++ src/main/scala/simple/Getter.scala
@@ -5,17 +5,21 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import java.util.concurrent.Executor
 import akka.pattern.pipe
+import akka.event.Logging
 
 object Getter {
   case object Done
   case object Abort
 }
 
-class Getter(url: String, depth: Int) extends Actor with ActorLogging {  
+class Getter(url: String, depth: Int) extends Actor {   
   import Getter._
   implicit val executor = context.dispatcher.asInstanceOf[Executor with ExecutionContext]
   def client: WebClient = AsyncWebClient
   
+  val log = Logging(context.system.eventStream, self.path.toString())
+  //val log = Logging(context.system.eventStream, this.getClass())
+
   val req_time = System.currentTimeMillis()
 
   client get url pipeTo self
@@ -24,8 +28,11 @@ class Getter(url: String, depth: Int) extends Actor with ActorLogging {
     case body: String =>
       val res_time = System.currentTimeMillis()
       val diff  = res_time - req_time
+      
+      log.warning(s"\n\n\t**** path = {} \n ", self.path)
+      log.info("\n\n\t $$$ Getter INFO LOG $$$")
       log.debug(s"\n\n\t**** $url took $diff miliseconds to response\n")
-      log.debug(s"\n\n\t**** path = {} \n ", self.path) 
+       
       for (link <- findLinks(body))
         context.parent ! Controller.Check(link, depth)
       context.stop(self)
